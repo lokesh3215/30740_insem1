11e07a630f86e45e40bc9b7c5f37fb96adb19b06
Lokesh Pantangi <pantangilokesh02@gmail.com>
2023-02-14 10:14:30 +0530

    Initial commit

---
+++ src/test/java/example/seleniumex3.java
@@ -0,0 +1,42 @@
+package example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class seleniumex3 {
+    @Test
+    public void test() {
+        WebDriver driver;
+        System.out.println("welcome");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.github.com/login");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.id("login_field")).sendKeys("2000030740@kluniversity.in");
+        highlight(driver,driver.findElement(By.id("login_field")));
+        driver.findElement(By.name("password")).sendKeys("welcome");
+        highlight(driver,driver.findElement(By.name("password")));
+        driver.findElement(By.className("header-logo"));
+        highlight(driver,driver.findElement(By.className("header-logo")));
+        //driver.findElement(By.linkText("Forgot password?")).click();
+        highlight(driver,driver.findElement(By.linkText("Forgot password?")));
+        //driver.findElement(By.partialLinkText("Create an account")).click();
+        driver.findElement(By.tagName("h1"));
+        highlight(driver,driver.findElement(By.tagName("h1")));
+        driver.findElement(By.xpath("//input[@id='login_field']"));
+        highlight(driver, driver.findElement(By.xpath("//input[@id='login_field']")));
+
+    }
+    public static void highlight(WebDriver driver, WebElement element) {
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
+    }
+}
